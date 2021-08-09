47f329d13fa996d1a16f353d7e9946ac4122d72f
georgiedekker <georgiedekker@gmail.com>
2021-08-09 15:15:34 +0200

    second commit

---
+++ chat/db/db.js
@@ -0,0 +1,101 @@
+const { throws } = require('assert');
+const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser');
+const { MongoClient } = require('mongodb');
+const { allowedNodeEnvironmentFlags } = require('process');
+let message = {}
+let user = {}
+let room = 'noRoom'
+let mis = {}
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.unsubscribe(bodyParser.json())
+app.use(cors({
+              origin: [
+              'http://localhost:3000',
+              'http://localhost:2700',
+              'http://localhost:3010']
+            }));
+
+app.get('/messages/', (req, res) => {
+              console.log('req: '+req+' res: '+res)
+
+              
+              mainLoad()
+              // .then(console.log)
+              .then(console.log('success'))
+              .catch(console.error)
+              .finally(() => client.close())
+              // console.log(JSON.stringify(mis))
+              // res.send(JSON.stringify(mes))
+              res.send(JSON.stringify(mis))
+              // res.send('<h1>Hey MondoDB</h1>');
+})
+app.post('/messages/', (req, res) => {
+              let mes = 'Message received on MongoDB node'
+              room  = req.body.message.room
+              message = req.body.message
+              user = req.body.user
+              console.log('received post: '+req.body.message+' with res: '+'res')
+              console.log('received post: '+req.body.user.name+' with res: '+'res')
+              console.log('received message from : '+user.name+' message: '+message)
+              main()
+              .then(console.log)
+              .catch(console.error)
+              .finally(() => client.close())
+              // console.log(Object.getOwnPropertyNames(req.body.user.name))
+              res.send(JSON.stringify(mes))
+              return message, user, room
+            })
+
+const url = 'mongodb+srv://chat:DLDA5wxUBYpu4vj@cluster0.dme9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// Database Name
+const colName = room
+const client = new MongoClient(url)
+// Database Name
+const dbName = 'Cluster0'
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect()
+  console.log('Connected successfully to server')
+  const db = client.db(dbName)
+  const collection = db.collection(colName)
+
+  // the following code examples can be pasted here...
+  const insertResult = await collection.insertOne({user:user, message:message})
+  console.log('Inserted documents =>', insertResult)
+
+//   const findResult = await collection.find({}).toArray()
+//               console.log('Found documents =>', findResult)
+
+  return 'done.'
+}
+
+async function mainLoad() {
+              // Use connect method to connect to the server
+              await client.connect()
+              console.log('Connected successfully to server')
+              const db = client.db(dbName)
+              const collection = db.collection(colName)
+            
+              // the following code examples can be pasted here...
+              // const insertResult = await collection.insertOne({user:user, message:message})
+              // console.log('Inserted documents =>', insertResult)
+            
+              const findResult = await collection.find({}).toArray()
+              //             console.log('Found documents =>', findResult)
+            mis = {result: findResult}
+              return mis
+            }
+
+
+
+
+app.listen(2700, function() {
+              console.log('listening on 2700')
+            })
--- chat/server/server.js
+++ chat/server/server.js
@@ -1,24 +1,54 @@
 const { throws } = require('assert');
 const { v4: uuidv4 } = require('uuid');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const bodyParser= require('body-parser')
+const fetch = require("node-fetch");
+// app.use(cors({
+//   origin: ['http://localhost:8080',
+//   'http://localhost:3000']
+// }));
+const app2 = express();
+app2.use(cors({
+  origin: ['http://localhost:8080',
+  'http://localhost:3000',
+  'http://localhost:2700',
+  'http://localhost:3010']
+}));
+app2.get('/room/', (req, res) => {
+  console.log('req: '+req+' res: '+res)
+  const payload = { room:defRoom}
+  res.type('json')
+  res.send(JSON.stringify(payload));
+});
 const http = require('http').createServer(app);
+
 const io = require('socket.io')(http, {
               cors: {
-                            origin: ['http://localhost:8080']
+                            origin: ['http://localhost:8080',
+                            'http://localhost:3000',
+                            'http://localhost:2700',
+                            'http://localhost:3010']
               }
 });
 
-const rooms = {  }
-const name = newUser('start')
-const userName = ''
-
+let history = {}
+let rooms = {  }
+let name = { }
+let userName = ''
+let messages = []
+let defRoom = 'start'
+app.use(bodyParser.json())
+app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   console.log('req: '+req+' res: '+res)
   res.send('<h1>Hey Socket.io</h1>');
 });
+
+
 
 app.post('/',  (req, res) => {
   res.send('<h1></h1>POST request to the homepage</h1>')
@@ -27,38 +57,117 @@ app.post('/',  (req, res) => {
 
 io.on('connection', (socket) => {
   //receive token from client
+  // socket.join(defRoom)
   let token = socket.handshake.auth.token;
   if(token){console.log('handshake succes '+token.toString())}
   let user = newUser(socket.handshake.name)
-  socket.emit('connection-success', user)
+  // socket.to(defRoom).emit('connection-success', user)
+  socket.emit('connection-succes', user)
+  io.emit('connection-succes', user)
   //user joins a room
-  socket.on('new-user', (userName) => {
-                console.log('new-user event on server: '+userName.name)
-                // room = Math.floor(Math.random()*10000)
-              user.name = userName.name
+  socket.on('new-user', (userName, room) => {
+                console.log('new-user event on server: '+userName)
+              user.name = userName
               user.timeStamp = Date.now()
+
+              getData('http://localhost:2700/messages/')
+                .then(data => {
+                  // console.log('obj keys data:'+Object.keys(data))
+                  // console.log(JSON.stringify(data))
+                  // console.log(data); // JSON data parsed by `data.json()` call
+                  // console.log(data)
+                  // console.log(data.result)
+                  // messages = data.result
+                  // socket.emit('messages', data)
+                  return data
+                })
+                // .finally((data) => socket.emit('messages', data));
+                
               //  console.log(user.id)
                 // socket.join(room)
                 // rooms[room] = { users: {} }
                 // rooms[room].users[socket.id] = name
-                socket.emit('user-connected', user)
+                // socket.to(room).emit('user-connected', user)
                 // socket.to(room).broadcast.emit('user-connected', name)
                 // socket.broadcast.emit('user-connected',name)
                 // if(room){console.log('rooms'+rooms[0].toString())}
                 // return rooms
+                // console.log('user object: '+Object.keys(user))
                 return user
               })
               
   //message that a user has sent
-  socket.on('send-chat-message', (message, user) => {
+  socket.on('send-chat-message', (message, usr) => {
                 // if(message){
                 user.timeStamp = Date.now()
-                console.log('message received on server: '+message+'from: '+user.name)
+                user.name = usr.name
+                user.rooms = usr.rooms
+                // defRoom = room
+                console.log('message received on server: '+message+'from: '+user.name+' '+Object.keys(user))
                 // socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
                 // socket.emit('chat-message',{message: message, user: user})
-                io.emit('chat-message',{message: message, user: user})
+                
+                const received = {message: message, user: user}
+                console.log('received.message: '+received.message)
+                postData('http://localhost:2700/messages/', received)
+                .then(data => {
+                  // console.log(received)
+                  console.log(data); // JSON data parsed by `data.json()` call
+                });
+                io.emit('chat-message', received)
+                socket.emit('chat-message',{message:'received your message',user:received.user})
                 // }
+                return user
               });
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// Example GET method implementation:
+async function getData(url = '') {
+  // Default options are marked with *
+  const response = await fetch(url)
+  .then(function(response) {
+    return response.text();
+  })
+  .then(function(data) {
+    // console.log(type(data));
+    
+    messages = JSON.parse(data)
+    socket.emit('messages', messages)
+    // console.log(messages.result)
+    return messages
+  });
+  // console.log(r)
+// console.log(response.statusText)
+// console.log(response.body)
+
+// const output = {}
+
+// if(response.body.json()){
+  // response = response.body.json()
+// }
+return messages
+  // return response.body.json(); // parses JSON response into native JavaScript objects
+}
+
+
 
   //when user disconnects
   socket.on('disconnect', () => {
@@ -74,6 +183,9 @@ io.on('connection', (socket) => {
 http.listen(3000, () => {
   console.log('listeninghttp on *:3000');
 });
+app2.listen(3010, () => {
+  console.log('listening for api calls on *:3010')
+})
 
 //Find the rooms where the current user is in
 // function getUserRooms(socket) {
@@ -85,12 +197,31 @@ http.listen(3000, () => {
           // } 
 
 function newUser(userName){
-  const user = {}
-    user.id = uuidv4(),
-    user.name = userName,
-    user.status = 'online',
-    user.typing = false,
-    user.timeStamp = Date.now(),
-    user.rooms = {}
+  let  user = {
+    id: uuidv4(),
+    name: userName,
+    status: 'online',
+    typing: false,
+    timeStamp: Date.now(),
+    rooms: {}
+  }
+    // console.log(Object.keys(user))
   return user
 }
+
+const TYPES = {
+  'undefined'        : 'undefined',
+  'number'           : 'number',
+  'boolean'          : 'boolean',
+  'string'           : 'string',
+  '[object Function]': 'function',
+  '[object RegExp]'  : 'regexp',
+  '[object Array]'   : 'array',
+  '[object Date]'    : 'date',
+  '[object Error]'   : 'error'
+},
+TOSTRING = Object.prototype.toString;
+
+function type(o) {
+  return TYPES[typeof o] || TYPES[TOSTRING.call(o)] || (o ? 'object' : 'null');
+};
